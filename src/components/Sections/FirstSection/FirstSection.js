afa36c7dffa81259f47b6f61af2b66542f08cb73
Guru-utvikling <damian@guru-utvikling.no>
2020-11-20 10:50:15 +0100

    Working header,first section

--- src/components/Sections/FirstSection/FirstSection.js
+++ src/components/Sections/FirstSection/FirstSection.js
@@ -4,17 +4,24 @@ const FirstSection = () => {
   return (
     <div className='container__section1'>
       <div className='container__header--left'>
-        <h1 className="header__title">Your business is our business</h1>
-        <p className="header__content">
+        <h1 className='header__title'>Your business
+        is our business</h1>
+        <p className='header__content'>
           Lorem Ipsum is simply dummy text of the printing and typesetting
           industry. Lorem Ipsum has been the industry's standard dummy text ever
           since the 1500s, when an unknown printer took a galley of type and
           scrambled it to make a type specimen book.
         </p>
-        <Link className="header__CTABtn" to='/'>FOR SELSKAPER</Link>
-        <Link className="header__CTABtn" to='/'>FOR ANSATTE</Link>
+        <div className="container__CTABtn">
+          <Link className='header__CTABtn' to='/'>
+            FOR SELSKAPER
+          </Link>
+          <Link className='header__CTABtn' to='/'>
+            FOR ANSATTE
+          </Link>
+        </div>
       </div>
-      <div className='header__right-column'></div>
+      <div className='container__header--right'></div>
     </div>
   )
 }
