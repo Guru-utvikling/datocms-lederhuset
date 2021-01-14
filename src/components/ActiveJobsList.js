c701de9ec798ba89bb9ef401493a1b0bb44d4815
Guru-utvikling <damian@guru-utvikling.no>
2021-01-14 14:46:28 +0100

    styling blogpreview on mainpage

--- src/components/ActiveJobsList.js
+++ src/components/ActiveJobsList.js
@@ -1,36 +1,55 @@
 import React from "react"
 import Masonry from "react-masonry-component"
-import {Link} from 'gatsby'
+import { Link } from "gatsby"
 const ActiveJobsList = (props) => {
   return (
-    <Masonry className='showcase'>
-      {Object.keys(props.activelist.data).map((item) => {
-        return (
-          <div key={props.activelist.data[item]} className='showcase__item'>
-            <figure className='card'>
-              <div>
-                <img
-                  width='100%'
-                  height='auto'
-                  src={props.activelist.data[item].logo}
-                />
-              </div>
-              <figcaption className='card__caption'>
-                <h1 className='card__title'>
-                  {props.activelist.data[item].name}
-                </h1>
-                <div className='card__description'>
-                  <p> <span className="small_bold">Start Date :</span> {props.activelist.data[item].startDate}</p>
-                  <p> <span className="small_bold">Salary :</span> {props.activelist.data[item].salary}</p>
-                  <p> <span className="small_bold">Ingress :</span> {props.activelist.data[item].ingress}</p>
-                  <Link className="employess__CTABtn" to={props.activelist.data[item].applyUrl}>Søk på stillingen</Link>
+    <div style={{padding:'5rem'}}>
+      <Masonry className='showcase'>
+        {Object.keys(props.activelist.data).map((item) => {
+          return (
+            <div key={props.activelist.data[item]} className='showcase__item'>
+              <figure className='card'>
+                <div>
+                  <img
+                    width='100%'
+                    height='auto'
+                    src={props.activelist.data[item].logo}
+                  />
                 </div>
-              </figcaption>
-            </figure>
-          </div>
-        )
-      })}
-    </Masonry>
+                <figcaption className='card__caption'>
+                  <h1 className='card__title'>
+                    {props.activelist.data[item].name}
+                  </h1>
+                  <div className='card__description'>
+                    <p>
+                      {" "}
+                      <span className='small_bold'>Start Date :</span>{" "}
+                      {props.activelist.data[item].startDate}
+                    </p>
+                    <p>
+                      {" "}
+                      <span className='small_bold'>Salary :</span>{" "}
+                      {props.activelist.data[item].salary}
+                    </p>
+                    <p>
+                      {" "}
+                      <span className='small_bold'>Ingress :</span>{" "}
+                      {props.activelist.data[item].ingress}
+                    </p>
+                    <Link
+                      className='employess__CTABtn'
+                      to={props.activelist.data[item].applyUrl}
+                    >
+                      Søk på stillingen
+                    </Link>
+                  </div>
+                </figcaption>
+              </figure>
+            </div>
+          )
+        })}
+      </Masonry>
+    </div>
   )
 }
 
--- src/components/ArtivclePreview.js
+++ src/components/ArtivclePreview.js
@@ -1,4 +1,5 @@
 import React from "react"
+import Masonry from "react-masonry-component"
 import { useStaticQuery, graphql, Link } from "gatsby"
 export default function Header() {
   const data = useStaticQuery(graphql`
@@ -10,24 +11,44 @@ export default function Header() {
             nyheterTitle
             nyheterExcerpt
             nyheterCoverimage {
-            fluid {
-              src
+              sizes(maxWidth: 300) {
+                src
+              }
             }
-          }
           }
         }
       }
     }
   `)
   return (
-    <div>
-      {data.allDatoCmsNyheter.edges.map(({ node: nyheter }) => (
-        <div>
-          <img src={nyheter.nyheterCoverimage.fluid.src}/>
-          <h2>{nyheter.nyheterTitle}</h2>
-          <p>{nyheter.nyheterExcerpt}</p>
-        </div>
-      ))}
+    <div className='container__articlePreview'>
+      <Masonry style={{ width: "1200px" }} className='showcase'>
+        {data.allDatoCmsNyheter.edges.map(({ node: nyheter }) => (
+          <div key={nyheter.id} className='showcase__item'>
+            <figure className='card'>
+              <Link to={`/nyheters/${nyheter.slug}`} className='card__image'>
+                <img src={nyheter.nyheterCoverimage.sizes.src} />
+              </Link>
+              <figcaption className='card__caption'>
+                <h6 className='card__title'>
+                  <Link to={`/nyheters/${nyheter.slug}`}>
+                    {nyheter.nyheterTitle}
+                  </Link>
+                </h6>
+                <div className='card__description'>
+                  <p>{nyheter.nyheterExcerpt}</p>
+                </div>
+              </figcaption>
+              <figcaption className='card__caption-lesmer'>
+                <Link className='lesmer-link' to={`/nyheters/${nyheter.slug}`}>
+                  Les mer>
+                </Link>
+              </figcaption>
+            </figure>
+          </div>
+        ))}
+      </Masonry>
+      <Link className="employess__CTABtn" to="/nyheter">Se alle innleggene</Link>
     </div>
   )
 }
